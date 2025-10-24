f3ea7bb92bd6c5e281096572aa10ed34c870c626
GabS744 <sousagabriel7444@gmail.com>
2025-10-24 19:06:06 -0300

    feat: project setup and initial configuration

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {  Container } from './style';
+import { NavBarMobile } from '../src/components/NavBar';
+
+const App: React.FC = () => (
+  <NavBarMobile></NavBarMobile>
+);
+
+export default App;
